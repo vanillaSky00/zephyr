438daec57331061ab556226038d94bdf93d70fce
vanilasky00 <pipichun2@gmail.com>
2026-01-22 22:59:31 +0800

    resolve dockerfile for missing dependency and write simple endpoint for observity of the result after rq is processed

--- backend/app/api/v1/timeline.py
+++ backend/app/api/v1/timeline.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from app.deps import get_db
-from app.domain.entries import repo as entries_repo
+from app.domain.entries import repo as entries_repo, models as entries_models
 
 router = APIRouter(prefix="/timeline", tags=["timeline"])
 
@@ -14,3 +14,9 @@ def create_entry(request: CreateEntryRequest, db: Session = Depends(get_db)):
     # TODO: remove hardcoded user_id
     entry = entries_repo.create_entry(db, user_id=1, entry_text=request.entry_text)
     return {"entry_id": entry.id}
+
+@router.get("/{entry_id}")
+def get_entry(entry_id: int, db: Session = Depends(get_db)) -> entries_models.Entry:
+    # TODO: remove hardcoded user_id
+    entry = entries_repo.get_entry(db, entry_id=entry_id, user_id=1)
+    return entry
